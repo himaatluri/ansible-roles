519b31bc0b7e2e8afa3136e99cb563b983e760a6
hima atluri <himain8977@gmail.com>
2019-06-16 11:19:43 -0500

    added printing statements

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+printf "[INFO]  Installing virtualenv python package.. make sure python version is 3 and up..\n"
 pip3 install virtualenv
 
+printf "\n[INFO] Enabling virtual env...."
 virtualenv ansible
 
+printf "\n[INFO] Activating virtual env...."
 source ansible/bin/activate
 
+printf "\n[INFO] Installing requirements...."
 ansible/bin/pip install -r requirements.txt
+
+printf "\n[INFO] Ansible env is setup...."
